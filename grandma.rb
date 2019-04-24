050178b83386358a2c20d56f1dd260c8eeb4de1b
Gian Libunao <gianlibunao@gmail.com>
2019-04-24 01:56:46 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,9 @@
 def speak_to_grandma(arg)
   if arg == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"  
-  else if arg == arg.upcase
+  elsif arg == arg.upcase
     "NO, NOT SINCE 1938!"
   else
   "HUH?! SPEAK UP, SONNY!"
-  end
   end
 end
